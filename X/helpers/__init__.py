f6ee485dee21f191411b55bc64702fa4c7f221ff
Premium <166982410+AtharvOP01@users.noreply.github.com>
2024-05-06 15:00:45 +0530

    Update __init__.py

--- X/helpers/__init__.py
+++ X/helpers/__init__.py
@@ -21,11 +21,11 @@ def restart():
 
 async def join(client):
     try:
-        await client.join_chat("SAIFALLBOT")
-        await client.join_chat("SAIFALLBOT")
-        await client.join_chat("SAIFALLBOT")
-        await client.join_chat("SAIFALLBOT")
-        await client.join_chat("SAIFALLBOT")
-        await client.join_chat("SAIFALLBOT")
+        await client.join_chat("PREMIUMXOP")
+        await client.join_chat("PREMIUMXOP")
+        await client.join_chat("PREMIUMXOP")
+        await client.join_chat("PREMIUMXOP")
+        await client.join_chat("PREMIUMXOP")
+        await client.join_chat("PREMIUMXOP")
     except BaseException:
         pass
